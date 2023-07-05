7f2169603a84e8de1c72ab5376de17fdb729133b
Matias <vallejosmatias732@gmail.com>
2023-07-04 21:39:05 -0300

    Estilos a pantalla de crear sala y navegacion a pantalla de puntos

--- src/screens/CreateRoomScreen.tsx
+++ src/screens/CreateRoomScreen.tsx
@@ -1,89 +1,119 @@
-import { StyleSheet, Text, TextInput, View } from "react-native"
+import { KeyboardAvoidingView, StyleSheet, Text, SafeAreaView , TextInput, TouchableOpacity, View, ScrollView } from 'react-native';
+import { RootStackParamList } from "../navigator/Navigator";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
-export const CreateRoomScreen = () => {
-  return (
-    
-    <View style={ styles.view }>
+type Props = NativeStackScreenProps<RootStackParamList, 'CreateRoomScreen'>;
+
+export const CreateRoomScreen = ( { navigation }: Props ) => {
+
+    const onPlayGame = () => {
+
+        navigation.navigate('PointsScreen');
+
+    }
+
+    return (        
         
-        <View 
-            style={{                
-                justifyContent: 'center'
-                ,alignItems: 'center'
-                ,marginBottom: 30
-            }}
-        >
+        
+        <SafeAreaView style={ styles.view }>
 
-            <Text
-                style={{
-                    borderWidth: 2
-                    ,borderColor: 'blue'
-                    ,borderRadius: 15
-                    ,padding: 20
-                    ,color: 'white'
-                    ,backgroundColor: 'blue'
-                    ,fontSize: 20                    
-                }}
-            >
-                ¡El nombre de la sala representa un ID único con el cúal los otros jugadores se van a poder conectar!
-            </Text>
+            <ScrollView>
 
-            <TextInput
-                style={ styles.input } 
-                placeholder="Nombre de la sala"
-            />
+                <KeyboardAvoidingView behavior={ 'height' }>
 
-        </View>
+                    <View 
+                        style={{                
+                            justifyContent: 'center'
+                            ,alignItems: 'center'
+                            ,marginBottom: 30                            
+                        }}
+                    >
 
-        <View 
-            style={{                
-                justifyContent: 'center'
-                ,alignItems: 'center'
-                ,marginTop: 30
-            }}
-        >
+                        <Text style={ styles.textLabel }>
+                            ¡El nombre de la sala representa un ID único con el cúal los otros jugadores se van a poder conectar!
+                        </Text>
 
-            <Text
-                style={{
-                    borderWidth: 2
-                    ,borderColor: 'blue'
-                    ,borderRadius: 15
-                    ,padding: 20
-                    ,color: 'white'
-                    ,backgroundColor: 'blue' 
-                    ,fontSize: 20                   
-                }}
-            >
-                ¡Vigila quien se conecta a tu sala! Determina una contraseña para conectarse a tu sala y empezar a jugar
-            </Text>
+                        <TextInput
+                            style={ styles.input } 
+                            placeholder="Nombre de la sala"
+                        />
 
-            <TextInput
-                style={ styles.input } 
-                placeholder="Contraseña (opcional)"
-            />
+                    </View>
 
-        </View>
+                    <View 
+                        style={{                
+                            justifyContent: 'center'
+                            ,alignItems: 'center'
+                            ,marginTop: 30
+                        }}
+                    >
 
-    </View>
+                        <Text style={ styles.textLabel }>
+                            ¡Vigila quien se conecta a tu sala! Determina una contraseña para conectarse a tu sala y empezar a jugar
+                        </Text>
 
-  )
+                        <TextInput
+                            style={ styles.input } 
+                            placeholder="Contraseña (opcional)"
+                        />
+
+                    </View>
+
+                    <View style={{ alignItems:'center' , marginTop: 40 }}>
+
+                        <TouchableOpacity
+                            style={ styles.button }
+                            onPress={ onPlayGame }
+                        >
+
+                            <Text style={{ fontSize: 25 , textAlign: 'center' }}>¡Empezar a jugar!</Text>
+
+                        </TouchableOpacity>                                
+
+                    </View>
+
+                </KeyboardAvoidingView>
+
+            </ScrollView>
+
+        </SafeAreaView>    
+
+    )
 }
 
 const styles = StyleSheet.create({
     view: {
-        flex: 1
-        ,alignItems: 'center'
+        flex: 1        
+        ,alignItems: 'center'        
         ,justifyContent: 'center'
+        ,padding: 15                
+        ,backgroundColor: '#87CEEB'
+    }
+    ,button: {
+        borderRadius: 25
+        ,borderColor: '#ffff'
+        ,borderWidth: 5
+        ,backgroundColor: 'transparent'
         ,padding: 15
-        // ,backgroundColor: 'red'
+        ,width: '70%' 
     }
     ,input: {
         height: 50
         ,width: '60%'
         ,textAlign: 'center'
-        ,fontSize: 25       
+        ,fontSize: 20       
         ,margin: 12
         ,borderBottomWidth: 2
-        ,borderBottomColor: 'red'
-        ,padding: 10
-      },
+        ,borderBottomColor: '#007BFF'
+        ,padding: 10        
+    }
+    ,textLabel: {
+        borderWidth: 2
+        ,borderColor: '#ffff'
+        ,borderRadius: 20
+        ,padding: 20
+        ,color: 'black'
+        ,backgroundColor: '#ffff' 
+        ,fontSize: 20
+    }
 });
